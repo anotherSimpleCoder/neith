cc1dd7c4c2ca4e66cbbe1f867d8c2fb1823bb674
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-09-27 09:55:42 +0200

    Added first @provide prototype

--- neith/neith-js-compiler.ts
+++ neith/neith-js-compiler.ts
@@ -5,9 +5,30 @@ export class NeithJSCompiler {
     private tsString: string = ''
     private counter: number = 0
 
+    provideCode(code: string) {
+        this.tsString = code + this.tsString
+    }
+
     handleScriptTag(element: NeithElement) {
         if(element.tag !== 'script') {
             throw Error("JSError: Invalid Tag!")
+        }
+
+        const stateDecRegex = /let\s+([a-zA-Z_]\w*)\s*:\s*([a-zA-Z_]\w*)\s*=\s*(.+);?/g
+        const stateDeclarations = element.text.match(stateDecRegex)
+        if(stateDeclarations) {
+            // element.text = element.text.replace(stateDecRegex, '')
+            for(const dec of stateDeclarations) {
+                const nameMatches = (dec as string).match(/let\s+([a-zA-Z_]\w*)\s*:\s*[a-zA-Z_]\w*\s*=/)
+                const valueMatches = (dec as string).match(/let\s+[a-zA-Z_]\w*\s*:\s*[a-zA-Z_]\w*\s*=\s*(['"].*?['"]|\S+);?/)
+                if(!nameMatches || !valueMatches) {
+                    throw new Error("SyntaxError: Invalid state declaration!")
+                }
+                const value = valueMatches[1]
+                const name = nameMatches[1]
+
+ 
+            }
         }
 
         this.tsString += `\n${element.text}`
